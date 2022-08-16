24356447120f8e9efd48cf4d4aa8bdaf701f05d5
AmilaDissanayake <2019cs039@stu.ucsc.lk>
2022-08-17 01:38:23 +0530

    Sign up updated

--- dinamu_paha/lib/Screens/subject_UI.dart
+++ dinamu_paha/lib/Screens/subject_UI.dart
@@ -11,32 +11,19 @@ class _Subject_UIState extends State<Subject_UI> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: DefaultTabController(
-        length: 3,
-        child: Scaffold(
-          appBar: AppBar(
-            title: Text('Dinamu paha - Maths',),
-            leading: IconButton(
-              icon: Icon(Icons.arrow_back,),
-              onPressed: (){},
-            ),
-            flexibleSpace: Image.asset('assets/images/img_5.webp',fit: BoxFit.cover,),
-            bottom: const TabBar(
-              indicatorColor: Colors.white,
-              tabs: [
-                Tab(child: Text('Questions',style: TextStyle(color: Colors.white,fontWeight: FontWeight.w500, fontSize: 16,),)),
-                Tab(child: Text('Materials',style: TextStyle(color: Colors.white,fontWeight: FontWeight.w500, fontSize: 16),)),
-                Tab(child: Text('Past Papers',style: TextStyle(color: Colors.white,fontWeight: FontWeight.w500, fontSize: 16),)),
-              ],
-            ),
-          ),
-          body: const TabBarView(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Dinamu paha - Maths',),
+          leading: Row(
             children: [
-              Icon(Icons.directions_car),
-              Icon(Icons.directions_transit),
-              Icon(Icons.directions_bike_rounded),
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Material(
+                  shape: CircleBorder(),
+                ),
+              ),
             ],
-          ),
+          )
         ),
       ),
     );
