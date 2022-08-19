398be10569e0ec3dafa375267ce2d557972142c0
AmilaDissanayake <2019cs039@stu.ucsc.lk>
2022-08-19 10:40:03 +0530

    Lesson_UI

---
+++ dinamu_paha/lib/Screens/Lesson_UI.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class lesson_UI extends StatefulWidget {
+  const lesson_UI({Key? key}) : super(key: key);
+
+  @override
+  State<lesson_UI> createState() => _lesson_UIState();
+}
+
+class _lesson_UIState extends State<lesson_UI> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: DefaultTabController(
+          length: 3,
+          child: Scaffold(
+            appBar: AppBar(
+              backgroundColor: Colors.deepPurple,
+              title: Text('දිනමු පහ - අප අවට සත්තු',),
+              leading: IconButton(
+                icon: Icon(Icons.arrow_back),
+                onPressed: (){},
+              ),
+              bottom: TabBar(
+                tabs: [
+                  Tab(child: Text("පාඩම", style: TextStyle(fontSize: 16),)),
+                  Tab(child: Text("අනුමාන \n  ප්‍රශ්න", style: TextStyle(fontSize: 16),)),
+                  Tab(child: Text("     පසුගිය \nවිභාග ප්‍රශ්න", style: TextStyle(fontSize: 16),)),
+                ],
+              ),
+            ),
+          ),
+        )
+    );
+  }
+}
