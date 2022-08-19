8050ca351ff3c88423d132c40ae1aecb42b993af
AmilaDissanayake <2019cs039@stu.ucsc.lk>
2022-08-19 23:17:07 +0530

    updated

--- dinamu_paha/lib/Screens/Sub_topic_UI.dart
+++ dinamu_paha/lib/Screens/Sub_topic_UI.dart
@@ -36,8 +36,8 @@ class _SubTopic_UIState extends State<SubTopic_UI> {
                 scrollDirection: Axis.vertical,
                 child: Container(
                   width: MediaQuery.of(context).size.width,
-                  padding: EdgeInsets.fromLTRB(0, 30, 0, 80),
-                  height: MediaQuery.of(context).size.height-256,
+                  padding: EdgeInsets.fromLTRB(0, 20, 0, 20),
+                  height: MediaQuery.of(context).size.height-319,
                   decoration: BoxDecoration(
                     color: Colors.cyanAccent.withOpacity(0.3),
                   ),
