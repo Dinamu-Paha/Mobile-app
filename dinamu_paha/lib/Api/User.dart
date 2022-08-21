3d1ee165aad81904a9e21547c793e3a0a3856ee3
AmilaDissanayake <2019cs039@stu.ucsc.lk>
2022-08-21 13:21:19 +0530

    test

---
+++ dinamu_paha/lib/Api/User.dart
@@ -0,0 +1,24 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+
+class User{
+  // late String firstName;
+  // late String lastName;
+  // late DateTime dob;
+  String email;
+  // late Int mobileNumber;
+  // late String homeDistrict;
+  // late String divisionalSector;
+  // late YearPicker examinationYear;
+  String password;
+  // late String school;
+  // late String districtOfSchool;
+  // late String role;
+class User {
+  late String email;
+  late String password;
+
+  User(this.email, this.password);
+}
+}
